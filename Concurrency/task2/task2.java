ee5bda221a25504bfa8dbea509988fdb4d8f570d
Никулин Павел <XpeHo3eM@gmail.com>
2025-04-30 23:33:15 +0400

    task2

---
+++ Concurrency/task2/task2.java
@@ -0,0 +1,21 @@
+package task2;
+
+import java.util.concurrent.CompletableFuture;
+
+public class task2 {
+    public static void main(String[] args) {
+        Account firstAccount = new Account(100_000);
+        Account secondAccount = new Account(100_000);
+
+        AccountThread firstThread = new AccountThread(firstAccount, secondAccount, 100);
+        AccountThread secondThread = new AccountThread(secondAccount, firstAccount, 100);
+
+        CompletableFuture.allOf(
+                CompletableFuture.runAsync(firstThread),
+                CompletableFuture.runAsync(secondThread)
+        ).join();
+
+        System.out.println("Cash balance of the first account: " + firstAccount.getCacheBalance());
+        System.out.println("Cash balance of the second account: " + secondAccount.getCacheBalance());
+    }
+}
